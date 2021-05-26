3d88ff07cad825793cc153ece7e77ce12248bb14
jalvares <janice_alvares@yahoo.com>
2021-05-26 19:24:04 +0100

    Added implementation and test

--- src/main/java/text/Translator.java
+++ src/main/java/text/Translator.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Translator {
 
-    /** Method encrypts each text word using shift cipher
+    /**
+     * Method encrypts each text word using shift cipher
+     *
      * @param text
      */
-    static void encrypt(String text){
+    static void encrypt(String text) {
         char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         InputStream is = Translator.class.getClassLoader().getResourceAsStream(text);
 
@@ -19,7 +22,18 @@ public class Translator {
             String lineText;
 
             while ((lineText = lineReader.readLine()) != null) {
+
                 System.out.println(lineText);
+                char[] textChar = lineText.toCharArray();
+                char[] cipherText = new char[textChar.length];
+                for (int i = 0; i < textChar.length; i++) {
+
+                   int retVal = Arrays.binarySearch(alphabet, textChar[i]);
+                   retVal = retVal + 3;
+                   cipherText[i] = alphabet[retVal];
+                   System.out.println(cipherText[i]);
+
+                }
             }
 
             lineReader.close();
@@ -28,11 +42,12 @@ public class Translator {
         }
     }
 
-    /** Method decrypts words.
+    /**
+     * Method decrypts words.
      *
      * @param text Cipher text
      */
-    static void decrypt(String text){
+    static void decrypt(String text) {
 
     }
 }
